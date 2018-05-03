d6ab7ffeaa0b558bd1a45d72854e3ce55bad14f1
Theodor Johannesen <tej@farpay.dk>
2018-05-03 12:53:21 +0100

    project example
    inital version - console application

---
+++ src/ConsumeWebhooksExample/ConsumeWebhooksExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeWebhooksExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
